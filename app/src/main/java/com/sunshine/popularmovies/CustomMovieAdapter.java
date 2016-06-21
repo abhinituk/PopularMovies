9cc495e69cc827a9a15c92935c8049936f582e7f
abhinituk <abhi481.007@gmail.com>
2016-06-22 00:49:58 +0530

    Enter Your API Key

--- app/src/main/java/com/sunshine/popularmovies/CustomMovieAdapter.java
+++ app/src/main/java/com/sunshine/popularmovies/CustomMovieAdapter.java
@@ -3,7 +3,6 @@ package com.sunshine.popularmovies;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v4.widget.CursorAdapter;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -55,11 +54,45 @@ public class CustomMovieAdapter extends CursorAdapter {
         ImageView poster= (ImageView) view.findViewById(R.id.grid_item_imageview);
 //        PosterImageViewHolder viewHolder = (PosterImageViewHolder) view.getTag();
         String posterPath= cursor.getString(cursor.getColumnIndex(MovieContract.MovieEntry.COLUMN_MOVIE_POSTER_PATH));
-        Log.v("PosterPath",posterPath);
+
+
+//         Target target = new Target() {
+//            @Override
+//            public void onBitmapLoaded(final Bitmap bitmap, Picasso.LoadedFrom from) {
+//                new Thread(new Runnable() {
+//                    @Override
+//                    public void run() {
+//
+//                        File file = new File(
+//                                Environment.getExternalStorageDirectory().getPath()
+//                                        + "/saved.jpg");
+//                        try {
+//                            file.createNewFile();
+//                            FileOutputStream ostream = new FileOutputStream(file);
+//                            bitmap.compress(Bitmap.CompressFormat.JPEG,100,ostream);
+//                            ostream.close();
+//                        }
+//                        catch (Exception e) {
+//                            e.printStackTrace();
+//                        }
+//                    }
+//                }).start();
+//            }
+//
+//            @Override
+//            public void onBitmapFailed(Drawable errorDrawable) {}
+//
+//            @Override
+//            public void onPrepareLoad(Drawable placeHolderDrawable) {}
+//        };
         Picasso.with(mContext)
                 .load(posterPath)
                 .placeholder(R.drawable.placeholder)
                 .error(R.drawable.placeholder_error)
                 .into(poster);
+
     }
+
+
+
 }
